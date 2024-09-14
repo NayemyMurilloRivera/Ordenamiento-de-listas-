eadc5d9a21e4b0150acb89d6de712ee57d1235af
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-09-14 04:51:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,11 @@ void merge1(int* inicio, int* medio, int* final) {
     int* ptr_medio = medio;
     int* ptr_final = final;
 
-    bool continuar = true;
-
-    while (ptr_medio < ptr_final +1  && continuar) {
+    while (ptr_medio < ptr_final +1) {
         for (int* p = ptr_inicio; p < ptr_final; p++) {
-            while (ptr_medio < ptr_final +1  && *p > *ptr_medio) {
-                if (*p > *ptr_medio) {
-                    swap(*p, *ptr_medio);
-                        break;
-                }
-                   
+            while (*p > *ptr_medio) {
+                swap(*p, *ptr_medio);
+                break;
                 if (*p == *ptr_medio) {
                     ptr_medio++;
                     break;
@@ -32,7 +27,7 @@ void merge1(int* inicio, int* medio, int* final) {
 }
 
 int main() {
-    int lista[10] = { 2,6,8 ,12, 20,1, 3,7, 11,15 };
+    int lista[10] = { 2,6,8 ,10, 20, 1, 3,7, 11,15 };
     int* ptr_inicio = lista;
     int* ptr_medio = lista + 5;
     int* ptr_final = lista + 9;
