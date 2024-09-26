a9c348e117da5663d36a670de061d062e0456e73
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-09-25 23:39:57 -0500

    Update palindromo.cpp

--- palindromo.cpp
+++ palindromo.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+void swapcreado(int* lugarini, int* lugarfinal)
+
+void split(*int  inicio, *int final){
+    while(inicio<final){
+        for(int* r= final; r<inicio; r--){
+            if(*inicio%2!=0){
+                swapcreado(*inicio,*final);
+                
+                
+
+
+            }
+        }
+    }
+
+
+
+}
