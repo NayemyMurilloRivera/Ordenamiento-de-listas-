247725eaa48d3a42f69f3db79dfe86c9a71fa9b6
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-09-28 08:33:15 -0500

    Actualizar split.cpp

--- split.cpp
+++ split.cpp
@@ -1,42 +1,81 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-void split( int * inicio , int * final){
-  int * p=inicio;
-  int * q= final; 
-  while (p< q ){
-     if (*p % 2!=0){
-        int  temp = *p;
-        for ( int * r = p ; r <q ; r++){
-            *r = *(r+1); 
+void split(int* inicio, int* final) {
+    int *primerImpar = final;
+    bool sigue = true;
+    while(inicio < final || sigue==true){
+        sigue = false;
+        for (int* p = inicio; p < final; ) {
+            if (*p % 2 != 0) {
+                int valorImpar = *p;
+                for (int* q = p; q < final; q++) {
+                
+                    if ( *(q+1) == 9) {
+                        sigue=false;
+                    }
+                    *q = *(q + 1);
+                
+                }
+                *(final - 1) = valorImpar;
+                final = primerImpar;
+                sigue = true;
+            
+            }
+            else {
+                p++;
+            }
         }
-        *q=temp;
-     }
-     else {
-        p++;
-     
-     }
- }    
- 
- int main() {
-    int lista[10] = {1,2,3,4,5,6,7,8,9,10};
+    }
+}
+
+
+void split1(int* inicio, int* final) {
+    //int *primerImpar = final;
+        for (int* p = inicio; p < final-1;) {
+            if (*p % 2 != 0) {
+                int valorImpar = *p;
+                for (int* q = p; q < final; q++) {
+                   /* if ( *(q+1) < *(final-1)) {
+                        sigue=false;
+                    }*/
+                   *q = *(q + 1);
+                
+                }
+                *(final - 1) = valorImpar;
+                final --;
+                
+            
+            }
+            else {
+                p++;
+            }
+        }
+    
+}
+
+
+
+int main() {
+    int lista[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int* ptr_inicio = lista;
-    int* ptr_medio = lista + 5;
-    int longi = obtener_longitud_array(ptr_inicio, ptr_medio);  
-    int* ptr_final = ptr_inicio + longi; 
+    int* ptr_final = lista + 10;
 
     cout << "Arreglo inicial:" << endl;
-    for (int* B = ptr_inicio; B < ptr_final; B++) {  
+    for (int* B = ptr_inicio; B < ptr_final; B++) {
         cout << "[" << *B << "] ";
     }
     cout << endl;
 
-    split(ptr_inicio, ptr_final ); 
+    split1(ptr_inicio, ptr_final);
+    split(ptr_inicio, ptr_final);
 
-    cout << "Arreglo resultante:" << endl;
-    for (int* B = ptr_inicio; B < ptr_final; B++) {  
+
+    cout << "Arreglo final:" << endl;
+    for (int* B = ptr_inicio; B < ptr_final; B++) {
         cout << "[" << *B << "] ";
     }
+    cout << endl;
 
     return 0;
 }
