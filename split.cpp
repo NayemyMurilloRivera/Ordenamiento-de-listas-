156576f22945bcc80eb0055cb8429b3646e6c345
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-09-28 09:50:03 -0500

    Update split.cpp

--- split.cpp
+++ split.cpp
@@ -1,65 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void split(int* inicio, int* final) {
-    int *primerImpar = final;
-    bool sigue = true;
-    while(inicio < final || sigue==true){
-        sigue = false;
-        for (int* p = inicio; p < final; ) {
-            if (*p % 2 != 0) {
-                int valorImpar = *p;
-                for (int* q = p; q < final; q++) {
-                
-                    if ( *(q+1) == 9) {
-                        sigue=false;
-                    }
-                    *q = *(q + 1);
-                
-                }
-                *(final - 1) = valorImpar;
-                final = primerImpar;
-                sigue = true;
-            
-            }
-            else {
-                p++;
+
+void split1(int* inicio, int* final) {
+    int* f = final;
+    for (int* p = inicio; p < f; p++, f--) {
+        swap(p, f);
+    }
+    final = f;
+    int* inicioImpar = final;
+
+    for (int* q = inicio; q < final; q++) {
+        if (q < inicioImpar) {
+            if (*q % 2 != 0) {
+                swap(*q, *inicioImpar);
+                inicioImpar--;
             }
         }
     }
-}
-
-
-void split1(int* inicio, int* final) {
-    //int *primerImpar = final;
-        for (int* p = inicio; p < final-1;) {
-            if (*p % 2 != 0) {
-                int valorImpar = *p;
-                for (int* q = p; q < final; q++) {
-                   /* if ( *(q+1) < *(final-1)) {
-                        sigue=false;
-                    }*/
-                   *q = *(q + 1);
-                
-                }
-                *(final - 1) = valorImpar;
-                final --;
-                
-            
-            }
-            else {
-                p++;
-            }
-        }
+    
     
 }
 
 
 
 int main() {
-    int lista[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int lista[9] = {9, 7, 5 , 3 , 1 , 2, 4 , 8, 10};
+    //int lista[5] = { 1, 3 , 5 , 7 , 10 };
+    //int lista[8] = { 1, 1 , 3 , 5 ,7, 9, 15, 10 };
     int* ptr_inicio = lista;
-    int* ptr_final = lista + 10;
+    int* ptr_final = lista + 9;
+    //int* ptr_final = lista + 8;
+
 
     cout << "Arreglo inicial:" << endl;
     for (int* B = ptr_inicio; B < ptr_final; B++) {
@@ -68,7 +40,6 @@ int main() {
     cout << endl;
 
     split1(ptr_inicio, ptr_final);
-    split(ptr_inicio, ptr_final);
 
 
     cout << "Arreglo final:" << endl;
