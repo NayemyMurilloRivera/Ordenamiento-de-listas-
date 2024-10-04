11510ccce7dc6f7ffcb166f511468c8345f98da5
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-10-04 13:37:36 -0500

    Update split.cpp

--- split.cpp
+++ split.cpp
@@ -3,35 +3,36 @@ using namespace std;
 
 
 void split1(int* inicio, int* final) {
-    int* f = final;
-    for (int* p = inicio; p < f; p++, f--) {
-        swap(p, f);
-    }
-    final = f;
-    int* inicioImpar = final;
-
-    for (int* q = inicio; q < final; q++) {
-        if (q < inicioImpar) {
-            if (*q % 2 != 0) {
-                swap(*q, *inicioImpar);
-                inicioImpar--;
-            }
+    int* f = final - 1;
+    while (inicio < f) {
+        for (int* i = inicio; i < f; f--) {
+            swap(*i, *f);
         }
     }
+    int* impar = final - 1;
+    int* p = inicio;
+
+    while (p < impar) {
+        if (*p % 2 != 0) {
+            swap(*p, *impar);
+            impar--;
+        }
+        else {
+            p++;
+            
+        }
+    }
+
+    final = f;
+
     
-    
+
 }
 
-
-
 int main() {
-    int lista[9] = {9, 7, 5 , 3 , 1 , 2, 4 , 8, 10};
-    //int lista[5] = { 1, 3 , 5 , 7 , 10 };
-    //int lista[8] = { 1, 1 , 3 , 5 ,7, 9, 15, 10 };
+    int lista[8] = { 1, 2, 3, 4, 5, 6 , 7 , 8};
     int* ptr_inicio = lista;
-    int* ptr_final = lista + 9;
-    //int* ptr_final = lista + 8;
-
+    int* ptr_final = lista + 8;
 
     cout << "Arreglo inicial:" << endl;
     for (int* B = ptr_inicio; B < ptr_final; B++) {
@@ -40,8 +41,6 @@ int main() {
     cout << endl;
 
     split1(ptr_inicio, ptr_final);
-
-
     cout << "Arreglo final:" << endl;
     for (int* B = ptr_inicio; B < ptr_final; B++) {
         cout << "[" << *B << "] ";
