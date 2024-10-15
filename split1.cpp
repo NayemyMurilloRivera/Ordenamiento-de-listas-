d7a317b6b07273be520af6974b6c76515a394954
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-10-15 00:01:50 -0500

    Update split1.cpp

--- split1.cpp
+++ split1.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// Función para separar los pares e impares y "ordenarlos" de manera secuencial
 void split(int* inicio, int* final) {
     int* impar = final - 1;
     int* par = inicio;
-
-    // Primera parte: separar pares e impares
     while (par < impar) {
         if (*par % 2 == 0) {
-            par++;  // Avanza si es par
+            par++;  
         } else {
-            swap(*par, *impar);  // Coloca los impares al final
+            swap(*par, *impar);  
             impar--;
         }
     }
 
-    // Segunda parte: Reorganizar los pares de forma secuencial
     for (int* i = inicio; i < par - 1; i++) {
         for (int* j = i + 1; j < par; j++) {
             if (*i > *j) {
@@ -25,7 +21,6 @@ void split(int* inicio, int* final) {
         }
     }
     
-    // Tercera parte: Reorganizar los impares de forma secuencial
     for (int* i = par; i < final - 1; i++) {
         for (int* j = i + 1; j < final; j++) {
             if (*i > *j) {
@@ -45,8 +40,7 @@ int main() {
         cout << "[" << *B << "] ";
     }
     cout << endl;
-    
-    // Separar pares e impares y luego organizarlos secuencialmente
+
     split(ptr_inicio, ptr_final);
     
     cout << "Arreglo final (pares e impares ordenados):" << endl;
