fb9347ea06ffb1c1fb2cd788e7021fc5d92736c7
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-11-02 09:30:45 -0500

    Actualizar merge_dinamico.cpp

--- merge_dinamico.cpp
+++ merge_dinamico.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -34,36 +35,31 @@ void merge(nodo*& A, nodo*& B) {
         j = j->next;
     }
 
-    for (nodo* p = inicio; i != nullptr || j != nullptr; p = p->next) {
+    // Realizar la fusión
+    nodo* p = inicio;
+    while (i != nullptr || j != nullptr) {
         if (i != nullptr && (j == nullptr || i->valor < j->valor)) {
             p->next = i;
             i = i->next;
-
         }
         else if (j != nullptr) {
             p->next = j;
             j = j->next;
         }
+        p = p->next;
     }
 
-    if (inicio == i) {
-        A = inicio;
-        B = nullptr;
-    }
-    else {
-        A = 
-        B = inicio;
-    }
-    cout << endl;
+    // Asigna el inicio de la lista fusionada a `A` y vacía `B`
+    A = inicio;
+    B = nullptr;  // Vaciar B después de la fusión
 }
-
 
 void creacion_nodo(int* begin, int* end, nodo*& nodo1) {
     for (int* i = end; i >= begin; i--)
         nodo1 = new nodo(*i, nodo1);
 }
 
-void impresion_nodo(nodo*& nodo1) {
+void impresion_nodo(nodo* nodo1) {
     cout << "Head->";
     nodo* tmp = nodo1;
     while (tmp) {
@@ -76,22 +72,27 @@ void impresion_nodo(nodo*& nodo1) {
 int main() {
     nodo* nodo1 = nullptr;
     nodo* nodo2 = nullptr;
-    int A[6] = { 13,17,20,25,30,38 };
-    int B[9] = {1,8,13,20,23,31,38,40,53};
+    int A[6] = { 13, 17, 20, 25, 30, 38 };
+    int B[9] = { 1, 8, 13, 20, 23, 31, 38, 40, 53 };
 
     creacion_nodo(A, A + 5, nodo1);
     creacion_nodo(B, B + 8, nodo2);
-
 
     cout << "Lista A: " << endl;
     impresion_nodo(nodo1);
     cout << "Lista B: " << endl;
     impresion_nodo(nodo2);
-    cout << "Merge:";
-    merge(nodo1, nodo2);
-    //impresion_nodo(nodo1);
-    impresion_nodo(nodo2);
 
+    cout << "Merge:" << endl;
+    merge(nodo1, nodo2);
+
+    // Muestra el resultado de la fusión
+    cout << "Lista fusionada (A): ";
+    impresion_nodo(nodo1);
+
+    // Verificar que B esté vacío
+    cout << "Lista B después del merge: ";
+    impresion_nodo(nodo2);
 
     return 0;
 }
