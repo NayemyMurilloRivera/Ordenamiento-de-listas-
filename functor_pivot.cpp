c49b6f04c557e82d77b2ccdeb3c1b8c1bdd4bf5e
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2024-11-25 16:33:49 -0500

    Update functor_pivot.cpp

--- functor_pivot.cpp
+++ functor_pivot.cpp
@@ -13,10 +13,8 @@ struct desc {
     }
 };
 
-
 template <class O>
 class pivot {
-    
 public:
     O decision;
     int pivote;
@@ -25,54 +23,49 @@ public:
         return decision(valor, pivote);
     }
 };
+
 template <class T, class FunctorPivot>
 void transformar(T* inicio, T* fin, FunctorPivot functor) {
     T* i = inicio;
     T* j = fin;
-    int* position= nullptr;
-    for (int* a = i; a < j; ) {
-        bool opcion = true;
-        if (functor(*a)) {
-            a++;
-        }
-        if (*a==functor.pivote) {
-            position = a;
-            a++;
-        }
-        else {
-            
-            while (opcion) {
-                if (!functor(*j)) {
-                    j--;
-                }
-                if (*j == functor.pivote) {
-                    position =j;
-                    j--;
 
-                }
-                else {
-                    opcion = false;
-                    swap(*j, *a);
-
-                }
-            }
+    // Buscar la posición del pivote
+    T* posPivote = nullptr;
+    for (T* ptr = inicio; ptr <= fin; ++ptr) {
+        if (*ptr == functor.pivote) {
+            posPivote = ptr;
+            break;
         }
     }
 
-    if (functor(*j)) {
-        swap(*j, *position);
-        cout << *j << endl;
+    if (!posPivote) {
+        cout << "Error: Pivote no encontrado en el arreglo.\n";
+        return;
     }
+
+    // Mover el pivote al final temporalmente
+    swap(*posPivote, *fin);
+
+    // Reorganizar elementos alrededor del pivote
+    T* nuevaPosPivote = inicio;
+    for (T* ptr = inicio; ptr < fin; ++ptr) {
+        if (functor(*ptr)) {
+            swap(*ptr, *nuevaPosPivote);
+            ++nuevaPosPivote;
+        }
+    }
+
+    // Colocar el pivote en su posición final
+    swap(*nuevaPosPivote, *fin);
 }
 
-
 int main() {
-
-    int arr[] = { 1 , 13 , 28 , 15 , 3 , 5 , 30 , 8 };
+    int arr[] = { 1, 13, 28, 15, 3, 5, 30, 8 };
     int n = sizeof(arr) / sizeof(arr[0]);
-    int pivote = 15;
+    int pivote = 8;
 
-    pivot<asc> ordenarAsc( pivote);
+    // Orden ascendente
+    pivot<asc> ordenarAsc(pivote);
     transformar(arr, arr + n - 1, ordenarAsc);
 
     for (int* ptr = arr; ptr != arr + n; ++ptr) {
@@ -80,12 +73,11 @@ int main() {
     }
     cout << endl;
 
-    int arr2[] = { 1 , 13 , 28 , 15 , 3 , 5 , 30 , 8 };
-    pivot<desc> ordenarDesc( pivote);
-
-
+    // Orden descendente
+    int arr2[] = { 1, 13, 28, 15, 3, 5, 30, 8 };
+    pivot<desc> ordenarDesc(pivote);
     transformar(arr2, arr2 + n - 1, ordenarDesc);
-    ;
+
     for (int* ptr = arr2; ptr != arr2 + n; ++ptr) {
         cout << *ptr << " ";
     }
